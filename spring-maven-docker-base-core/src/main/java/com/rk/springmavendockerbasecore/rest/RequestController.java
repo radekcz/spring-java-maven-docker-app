c2ff09d5b5bd7629ed7ecd38e874893399dcae8c
Radek Krizka <radek.krizka@pricefx.com>
2024-02-01 21:56:00 +0100

    add logging to rest controller

--- spring-maven-docker-base-core/src/main/java/com/rk/springmavendockerbasecore/rest/RequestController.java
+++ spring-maven-docker-base-core/src/main/java/com/rk/springmavendockerbasecore/rest/RequestController.java
@@ -1,16 +1,19 @@
 package com.rk.springmavendockerbasecore.rest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping(path = "api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class RequestController {
 
     @GetMapping(value = "hello")
     public String hello() {
+        log.info("Hello from /api/hello.");
         return "hello";
     }
 }

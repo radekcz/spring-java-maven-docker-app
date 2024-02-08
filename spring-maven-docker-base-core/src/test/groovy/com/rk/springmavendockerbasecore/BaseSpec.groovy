00986cad6c128f1aabcbc4bb58b92bbdcc720dd6
Radek Krizka <radek.krizka@pricefx.com>
2024-02-08 20:26:39 +0100

    add first groovy test

---
+++ spring-maven-docker-base-core/src/test/groovy/com/rk/springmavendockerbasecore/BaseSpec.groovy
@@ -0,0 +1,16 @@
+package com.rk.springmavendockerbasecore
+
+import spock.lang.Specification
+
+class BaseSpec extends Specification {
+
+    def "should run test with result #expectedResult"() {
+        expect:
+        a * b == expectedResult
+
+        where:
+        a   |   b   |  expectedResult
+        1   |   1   |  1
+        3   |   3   |  9
+    }
+}

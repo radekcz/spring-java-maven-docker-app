ef9ae68d07bb134222dcbb07a7b792625a6cc0f2
Radek Krizka <radek.krizka@pricefx.com>
2024-02-15 18:56:54 +0100

    add K8sResourceControllerSpec test

---
+++ core/src/test/groovy/com/rk/springmavendockerbasecore/rest/K8sResourceControllerSpec.groovy
@@ -0,0 +1,22 @@
+package com.rk.springmavendockerbasecore.rest
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@WebMvcTest(controllers = BaseController)
+class K8sResourceControllerSpec extends Specification {
+
+    @Autowired
+    MockMvc mvc
+
+    def "should return pods"() {
+        expect:
+        mvc.perform(get("/api/v1/pods"))
+                .andExpect(status().isOk())
+    }
+}

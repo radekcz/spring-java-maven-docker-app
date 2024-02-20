cd4dcc05be4e9334c1d620de66f6a7a6cfae6940
Radek Krizka <radek.krizka@pricefx.com>
2024-02-20 19:34:32 +0100

    add tests for profile endpoint

--- core/src/test/groovy/com/rk/springmavendockerbasecore/rest/BaseControllerSpec.groovy
+++ core/src/test/groovy/com/rk/springmavendockerbasecore/rest/BaseControllerSpec.groovy
@@ -6,6 +6,7 @@ import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
@@ -14,6 +15,18 @@ class BaseControllerSpec extends Specification {
 
     @Autowired
     MockMvc mvc
+
+    def "set profile endpoint returns bad request when parameter profile is missing"() {
+        expect:
+        mvc.perform(post("/api/v1/profile"))
+                .andExpect(status().is4xxClientError())
+    }
+
+    def "should set profile"() {
+        expect:
+        mvc.perform(post("/api/v1/profile?profile=QA"))
+                .andExpect(status().isOk())
+    }
 
     def "ping endpoint should return message"() {
         expect:

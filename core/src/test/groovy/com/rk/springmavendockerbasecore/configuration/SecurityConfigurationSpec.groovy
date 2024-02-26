e0bd9fba560af71560fd2071e69937aec8a856fa
Radek Krizka <radek.krizka@pricefx.com>
2024-02-26 11:21:34 +0100

    add SecurityConfigurationSpec

---
+++ core/src/test/groovy/com/rk/springmavendockerbasecore/configuration/SecurityConfigurationSpec.groovy
@@ -0,0 +1,26 @@
+package com.rk.springmavendockerbasecore.configuration
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class SecurityConfigurationSpec extends Specification {
+
+    @Autowired
+    private MockMvc mockMvc
+
+    def "allow access to unsecured endpoint"() {
+        expect:
+        mockMvc.perform(get("/actuator/health"))
+                .andExpect(status().isOk())
+    }
+}
